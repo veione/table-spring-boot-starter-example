2f265bd969444878ee7bcc4ea2860a2655c89976
weixiaoqiang <544483342@qq.com>
2024-05-10 17:48:58 +0800

    opt:移除多余的注解

--- src/main/java/com/think/table/TableSpringBootStarterExampleApplication.java
+++ src/main/java/com/think/table/TableSpringBootStarterExampleApplication.java
@@ -1,11 +1,9 @@
 package com.think.table;
 
-import com.think.table.annotation.TableScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@TableScan
 public class TableSpringBootStarterExampleApplication {
 
     public static void main(String[] args) {
